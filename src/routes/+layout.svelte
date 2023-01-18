cac7c583aaad24494f655feb8b05f40ee46943c1
Leon <l.botros@cs.ru.nl>
2023-01-18 12:20:23 +0100

    fix: meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,15 @@
 
 <svelte:head>
     <title>PostGuard</title>
-    <html lang="en" />
-</svelte:head>
+    <meta
+        name="description"
+        content="PostGuard is an easy-to-use encryption protocol for email and files."
+    />
+    <meta
+        name="keywords"
+        content="Encryption, Usability, Privacy, Security, Identity-Based Encryption"
+    /></svelte:head
+>
 
 {#if !$isLoading}
     <Header />
