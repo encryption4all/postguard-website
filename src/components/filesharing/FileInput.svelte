4bcec6714b8022a1a3f8c4559cb3657bb438260d
Fay <61050421+FayKn@users.noreply.github.com>
2025-12-10 16:08:59 +0100

    Dropzone custom file in upload

--- src/components/filesharing/FileInput.svelte
+++ src/components/filesharing/FileInput.svelte
@@ -4,6 +4,7 @@
     import 'dropzone/dist/dropzone.css'
     import plusIcon from '$lib/assets/images/plusicon.svg'
     import yiviLogo from '$lib/assets/images/non-free/yivi-logo.svg'
+    import UploadedFileTemplate from '$lib/components/filesharing/UploadedFileTemplate.svelte'
 
     // Disable auto-discover to prevent Dropzone from automatically attaching to all .dropzone elements
     Dropzone.autoDiscover = false
@@ -30,12 +31,16 @@
 
     // handle all the Dropzone setup in onMount to ensure it only runs in the browser
     onMount(() => {
+        // @ts-ignore, it's always set if UploadedFileTemplate is set because it's SSR'd
+        let previewTemplate = document.querySelector("#template").parentNode.innerHTML;
+
         myDropzone = new Dropzone('#my-form', {
             url: '#', // Dummy URL, can't be empty
             autoProcessQueue: false, // Prevent automatic upload
             addRemoveLinks: true,
             dictDefaultMessage: 'Drop files here or click to upload.',
             maxFilesize: maxFileSizeMB,
+            previewTemplate: previewTemplate,
         })
 
         myDropzone.on('addedfile', file => {
@@ -56,8 +61,11 @@
     })
 </script>
 <form id="my-form" class="dropzone">
+    <!-- so dropzone can get the template but its invisible -->
+    <div style="display: none"><UploadedFileTemplate /></div>
     <h1 style="margin-bottom: 8px">Securely send your files with:</h1>
     <img src={yiviLogo} alt="Yivi Logo" style="width: 120px; margin-bottom: 1rem;" />
+
     <div class="dz-message" data-dz-message>
         <img src={plusIcon} alt="Add files" />
         <h3>Drop files here or click to upload.</h3>
