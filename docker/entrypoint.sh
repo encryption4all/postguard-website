7534b2cf05001ed7373461ae575483ae72543ee1
Ruben Hensen <ruben.hensen@protonmail.com>
2026-02-24 20:37:45 +0100

    Use relative /pkg URL instead of VITE_PKG_URL env var
    PKG server is always proxied at /pkg relative to the app origin,
    so hardcode that path instead of requiring a runtime env variable.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,14 +3,10 @@ set -e
 
 HTDOCS="/usr/share/nginx/html/postguard"
 
-: "${VITE_PKG_URL:=/pkg}"
-
 echo "Substituting SPA environment variables..."
-echo "  VITE_PKG_URL=${VITE_PKG_URL}"
 echo "  VITE_FILEHOST_URL=${VITE_FILEHOST_URL}"
 
 find "${HTDOCS}" -name '*.js' -type f -exec sed -i \
-    -e "s|__VITE_PKG_URL_PLACEHOLDER__|${VITE_PKG_URL}|g" \
     -e "s|__VITE_FILEHOST_URL_PLACEHOLDER__|${VITE_FILEHOST_URL}|g" \
     {} +
 
